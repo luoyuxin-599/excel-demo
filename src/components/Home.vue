3a9d2229cec017f60388ae7f632735f71a4571ab
luoyuxin-599 <357057313@qq.com>
2020-12-23 17:00:54 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <excel :excelData="excelData" :dataName="dataName"></excel>
+  </div>
+</template>
+
+<script>
+import Excel from "./Excel";
+export default {
+  data() {
+    return {
+      excelData: [
+        //测试数据
+        {
+          name: "红烧鱼",
+          size: "大",
+          taste: "微辣",
+          price: "40",
+          remain: "100",
+        },
+        {
+          name: "麻辣小龙虾",
+          size: "大",
+          taste: "麻辣",
+          price: "138",
+          remain: "200",
+        },
+        {
+          name: "清蒸小龙虾",
+          size: "大",
+          taste: "清淡",
+          price: "138",
+          remain: "200",
+        }
+      ],
+      dataName:[{
+        name: "名称",
+        size: "分量",
+        taste: "口味",
+        price: "单价",
+        remain: "剩余(份)"
+      }]
+    };
+  },
+  components: {
+    Excel,
+  },
+};
+</script>
+
+<style>
+
+</style>
